3fada753cd756ca74a6b3bc8b0c15195bac9fb97
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2019-01-24 13:35:07 +0100

    ugui.h formatting fixes

--- common/adafruit_touch_display/ugui/ugui.h
+++ common/adafruit_touch_display/ugui/ugui.h
@@ -129,27 +129,6 @@ typedef UG_U16                                        UG_COLOR;
 #define ALIGN_TOP_CENTER                              (ALIGN_V_TOP|ALIGN_H_CENTER)
 #define ALIGN_TOP_LEFT                                (ALIGN_V_TOP|ALIGN_H_LEFT)
 
-/* Default IDs */
-#define OBJ_ID_0                                      0
-#define OBJ_ID_1                                      1
-#define OBJ_ID_2                                      2
-#define OBJ_ID_3                                      3
-#define OBJ_ID_4                                      4
-#define OBJ_ID_5                                      5
-#define OBJ_ID_6                                      6
-#define OBJ_ID_7                                      7
-#define OBJ_ID_8                                      8
-#define OBJ_ID_9                                      9
-#define OBJ_ID_10                                     10
-#define OBJ_ID_11                                     11
-#define OBJ_ID_12                                     12
-#define OBJ_ID_13                                     13
-#define OBJ_ID_14                                     14
-#define OBJ_ID_15                                     15
-#define OBJ_ID_16                                     16
-#define OBJ_ID_17                                     17
-#define OBJ_ID_18                                     18
-#define OBJ_ID_19                                     19
 
 /* -------------------------------------------------------------------------------- */
 /* -- FUNCTION RESULTS                                                           -- */
@@ -811,180 +790,183 @@ typedef struct
 #ifndef NORDIC_GUI
 UG_S16 UG_Init( UG_GUI* g, void (*p)(UG_S16,UG_S16,UG_COLOR), UG_S16 x, UG_S16 y );
 #endif
-UG_S16 UG_SelectGUI( UG_GUI* g );
-void UG_FontSelect( const UG_FONT* font );
-void UG_FillScreen( UG_COLOR c );
-void UG_FillFrame( UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c );
-void UG_FillRoundFrame( UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_S16 r, UG_COLOR c );
-void UG_DrawMesh( UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c );
-void UG_DrawFrame( UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c );
-void UG_DrawRoundFrame( UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_S16 r, UG_COLOR c );
-void UG_DrawPixel( UG_S16 x0, UG_S16 y0, UG_COLOR c );
-void UG_DrawCircle( UG_S16 x0, UG_S16 y0, UG_S16 r, UG_COLOR c );
-void UG_FillCircle( UG_S16 x0, UG_S16 y0, UG_S16 r, UG_COLOR c );
-void UG_DrawArc( UG_S16 x0, UG_S16 y0, UG_S16 r, UG_U8 s, UG_COLOR c );
-void UG_DrawLine( UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c );
-void UG_PutString( UG_S16 x, UG_S16 y, char* str );
-void UG_PutChar( char chr, UG_S16 x, UG_S16 y, UG_COLOR fc, UG_COLOR bc );
-void UG_ConsolePutString( char* str );
-void UG_ConsoleSetArea( UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye );
-void UG_ConsoleSetForecolor( UG_COLOR c );
-void UG_ConsoleSetBackcolor( UG_COLOR c );
-void UG_SetForecolor( UG_COLOR c );
-void UG_SetBackcolor( UG_COLOR c );
-UG_S16 UG_GetXDim( void );
-UG_S16 UG_GetYDim( void );
-void UG_FontSetHSpace( UG_U16 s );
-void UG_FontSetVSpace( UG_U16 s );
+UG_S16 UG_SelectGUI(UG_GUI * g);
+void UG_FontSelect(const UG_FONT * font);
+void UG_FillScreen(UG_COLOR c);
+void UG_FillFrame(UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c);
+void UG_FillRoundFrame(UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_S16 r, UG_COLOR c);
+void UG_DrawMesh(UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c);
+void UG_DrawFrame(UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c);
+void UG_DrawRoundFrame(UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_S16 r, UG_COLOR c);
+void UG_DrawPixel(UG_S16 x0, UG_S16 y0, UG_COLOR c);
+void UG_DrawCircle(UG_S16 x0, UG_S16 y0, UG_S16 r, UG_COLOR c);
+void UG_FillCircle(UG_S16 x0, UG_S16 y0, UG_S16 r, UG_COLOR c);
+void UG_DrawArc(UG_S16 x0, UG_S16 y0, UG_S16 r, UG_U8 s, UG_COLOR c);
+void UG_DrawLine(UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c);
+void UG_PutString(UG_S16 x, UG_S16 y, char * str);
+void UG_PutChar(char chr, UG_S16 x, UG_S16 y, UG_COLOR fc, UG_COLOR bc);
+void UG_ConsolePutString(char * str);
+void UG_ConsoleSetArea(UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye);
+void UG_ConsoleSetForecolor(UG_COLOR c);
+void UG_ConsoleSetBackcolor(UG_COLOR c);
+void UG_SetForecolor(UG_COLOR c);
+void UG_SetBackcolor(UG_COLOR c);
+UG_S16 UG_GetXDim(void);
+UG_S16 UG_GetYDim(void);
+void UG_FontSetHSpace(UG_U16 s);
+void UG_FontSetVSpace(UG_U16 s);
 
 /* Miscellaneous functions */
-void UG_WaitForUpdate( void );
-void UG_Update( void );
-void UG_DrawBMP( UG_S16 xp, UG_S16 yp, UG_BMP* bmp );
-void UG_TouchUpdate( UG_S16 xp, UG_S16 yp, UG_U8 state );
+void UG_WaitForUpdate(void);
+void UG_Update(void);
+void UG_DrawBMP(UG_S16 xp, UG_S16 yp, UG_BMP * bmp);
+void UG_TouchUpdate(UG_S16 xp, UG_S16 yp, UG_U8 state);
 
 /* Driver functions */
-void UG_DriverRegister( UG_U8 type, void* driver );
-void UG_DriverEnable( UG_U8 type );
-void UG_DriverDisable( UG_U8 type );
+void UG_DriverRegister(UG_U8 type, void * driver);
+void UG_DriverEnable(UG_U8 type);
+void UG_DriverDisable(UG_U8 type);
 
 /* Window functions */
-UG_RESULT UG_WindowCreate( UG_WINDOW* wnd, UG_OBJECT* objlst, UG_U8 objcnt, void (*cb)( UG_MESSAGE* ) );
-UG_RESULT UG_WindowDelete( UG_WINDOW* wnd );
-UG_RESULT UG_WindowShow( UG_WINDOW* wnd );
-UG_RESULT UG_WindowHide( UG_WINDOW* wnd );
-UG_RESULT UG_WindowResize( UG_WINDOW* wnd, UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye );
-UG_RESULT UG_WindowAlert( UG_WINDOW* wnd );
-UG_RESULT UG_WindowSetForeColor( UG_WINDOW* wnd, UG_COLOR fc );
-UG_RESULT UG_WindowSetBackColor( UG_WINDOW* wnd, UG_COLOR bc );
-UG_RESULT UG_WindowSetTitleTextColor( UG_WINDOW* wnd, UG_COLOR c );
-UG_RESULT UG_WindowSetTitleColor( UG_WINDOW* wnd, UG_COLOR c );
-UG_RESULT UG_WindowSetTitleInactiveTextColor( UG_WINDOW* wnd, UG_COLOR c );
-UG_RESULT UG_WindowSetTitleInactiveColor( UG_WINDOW* wnd, UG_COLOR c );
-UG_RESULT UG_WindowSetTitleText( UG_WINDOW* wnd, char* str );
-UG_RESULT UG_WindowSetTitleTextFont( UG_WINDOW* wnd, const UG_FONT* font );
-UG_RESULT UG_WindowSetTitleTextHSpace( UG_WINDOW* wnd, UG_S8 hs );
-UG_RESULT UG_WindowSetTitleTextVSpace( UG_WINDOW* wnd, UG_S8 vs );
-UG_RESULT UG_WindowSetTitleTextAlignment( UG_WINDOW* wnd, UG_U8 align );
-UG_RESULT UG_WindowSetTitleHeight( UG_WINDOW* wnd, UG_U8 height );
-UG_RESULT UG_WindowSetXStart( UG_WINDOW* wnd, UG_S16 xs );
-UG_RESULT UG_WindowSetYStart( UG_WINDOW* wnd, UG_S16 ys );
-UG_RESULT UG_WindowSetXEnd( UG_WINDOW* wnd, UG_S16 xe );
-UG_RESULT UG_WindowSetYEnd( UG_WINDOW* wnd, UG_S16 ye );
-UG_RESULT UG_WindowSetStyle( UG_WINDOW* wnd, UG_U8 style );
-UG_COLOR UG_WindowGetForeColor( UG_WINDOW* wnd );
-UG_COLOR UG_WindowGetBackColor( UG_WINDOW* wnd );
-UG_COLOR UG_WindowGetTitleTextColor( UG_WINDOW* wnd );
-UG_COLOR UG_WindowGetTitleColor( UG_WINDOW* wnd );
-UG_COLOR UG_WindowGetTitleInactiveTextColor( UG_WINDOW* wnd );
-UG_COLOR UG_WindowGetTitleInactiveColor( UG_WINDOW* wnd );
-char* UG_WindowGetTitleText( UG_WINDOW* wnd );
-UG_FONT* UG_WindowGetTitleTextFont( UG_WINDOW* wnd );
-UG_S8 UG_WindowGetTitleTextHSpace( UG_WINDOW* wnd );
-UG_S8 UG_WindowGetTitleTextVSpace( UG_WINDOW* wnd );
-UG_U8 UG_WindowGetTitleTextAlignment( UG_WINDOW* wnd );
-UG_U8 UG_WindowGetTitleHeight( UG_WINDOW* wnd );
-UG_S16 UG_WindowGetXStart( UG_WINDOW* wnd );
-UG_S16 UG_WindowGetYStart( UG_WINDOW* wnd );
-UG_S16 UG_WindowGetXEnd( UG_WINDOW* wnd );
-UG_S16 UG_WindowGetYEnd( UG_WINDOW* wnd );
-UG_U8 UG_WindowGetStyle( UG_WINDOW* wnd );
-UG_RESULT UG_WindowGetArea( UG_WINDOW* wnd, UG_AREA* a );
-UG_S16 UG_WindowGetInnerWidth( UG_WINDOW* wnd );
-UG_S16 UG_WindowGetOuterWidth( UG_WINDOW* wnd );
-UG_S16 UG_WindowGetInnerHeight( UG_WINDOW* wnd );
-UG_S16 UG_WindowGetOuterHeight( UG_WINDOW* wnd );
+UG_RESULT UG_WindowCreate(UG_WINDOW * wnd, UG_OBJECT * objlst, UG_U8 objcnt, void(*cb)(UG_MESSAGE*));
+UG_RESULT UG_WindowDelete(UG_WINDOW * wnd);
+UG_RESULT UG_WindowShow(UG_WINDOW * wnd);
+UG_RESULT UG_WindowHide(UG_WINDOW * wnd);
+UG_RESULT UG_WindowResize(UG_WINDOW * wnd, UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye);
+UG_RESULT UG_WindowAlert(UG_WINDOW * wnd);
+UG_RESULT UG_WindowSetForeColor(UG_WINDOW * wnd, UG_COLOR fc);
+UG_RESULT UG_WindowSetBackColor(UG_WINDOW * wnd, UG_COLOR bc);
+UG_RESULT UG_WindowSetTitleTextColor(UG_WINDOW * wnd, UG_COLOR c);
+UG_RESULT UG_WindowSetTitleColor(UG_WINDOW * wnd, UG_COLOR c);
+UG_RESULT UG_WindowSetTitleInactiveTextColor(UG_WINDOW * wnd, UG_COLOR c);
+UG_RESULT UG_WindowSetTitleInactiveColor(UG_WINDOW * wnd, UG_COLOR c);
+UG_RESULT UG_WindowSetTitleText(UG_WINDOW * wnd, char * str);
+UG_RESULT UG_WindowSetTitleTextFont(UG_WINDOW * wnd,
+    const UG_FONT * font);
+UG_RESULT UG_WindowSetTitleTextHSpace(UG_WINDOW * wnd, UG_S8 hs);
+UG_RESULT UG_WindowSetTitleTextVSpace(UG_WINDOW * wnd, UG_S8 vs);
+UG_RESULT UG_WindowSetTitleTextAlignment(UG_WINDOW * wnd, UG_U8 align);
+UG_RESULT UG_WindowSetTitleHeight(UG_WINDOW * wnd, UG_U8 height);
+UG_RESULT UG_WindowSetXStart(UG_WINDOW * wnd, UG_S16 xs);
+UG_RESULT UG_WindowSetYStart(UG_WINDOW * wnd, UG_S16 ys);
+UG_RESULT UG_WindowSetXEnd(UG_WINDOW * wnd, UG_S16 xe);
+UG_RESULT UG_WindowSetYEnd(UG_WINDOW * wnd, UG_S16 ye);
+UG_RESULT UG_WindowSetStyle(UG_WINDOW * wnd, UG_U8 style);
+UG_COLOR UG_WindowGetForeColor(UG_WINDOW * wnd);
+UG_COLOR UG_WindowGetBackColor(UG_WINDOW * wnd);
+UG_COLOR UG_WindowGetTitleTextColor(UG_WINDOW * wnd);
+UG_COLOR UG_WindowGetTitleColor(UG_WINDOW * wnd);
+UG_COLOR UG_WindowGetTitleInactiveTextColor(UG_WINDOW * wnd);
+UG_COLOR UG_WindowGetTitleInactiveColor(UG_WINDOW * wnd);
+char * UG_WindowGetTitleText(UG_WINDOW * wnd);
+UG_FONT * UG_WindowGetTitleTextFont(UG_WINDOW * wnd);
+UG_S8 UG_WindowGetTitleTextHSpace(UG_WINDOW * wnd);
+UG_S8 UG_WindowGetTitleTextVSpace(UG_WINDOW * wnd);
+UG_U8 UG_WindowGetTitleTextAlignment(UG_WINDOW * wnd);
+UG_U8 UG_WindowGetTitleHeight(UG_WINDOW * wnd);
+UG_S16 UG_WindowGetXStart(UG_WINDOW * wnd);
+UG_S16 UG_WindowGetYStart(UG_WINDOW * wnd);
+UG_S16 UG_WindowGetXEnd(UG_WINDOW * wnd);
+UG_S16 UG_WindowGetYEnd(UG_WINDOW * wnd);
+UG_U8 UG_WindowGetStyle(UG_WINDOW * wnd);
+UG_RESULT UG_WindowGetArea(UG_WINDOW * wnd, UG_AREA * a);
+UG_S16 UG_WindowGetInnerWidth(UG_WINDOW * wnd);
+UG_S16 UG_WindowGetOuterWidth(UG_WINDOW * wnd);
+UG_S16 UG_WindowGetInnerHeight(UG_WINDOW * wnd);
+UG_S16 UG_WindowGetOuterHeight(UG_WINDOW * wnd);
 
 /* Button functions */
-UG_RESULT UG_ButtonCreate(UG_BUTTON* btn, UG_WINDOW* wnd, UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye );
-UG_RESULT UG_ButtonDelete(UG_BUTTON *btn);
-UG_RESULT UG_ButtonShow(UG_BUTTON *btn);
-UG_RESULT UG_ButtonHide(UG_BUTTON *btn);
-UG_RESULT UG_ButtonSetForeColor(UG_BUTTON *btn, UG_COLOR fc );
-UG_RESULT UG_ButtonSetBackColor(UG_BUTTON *btn, UG_COLOR bc );
-UG_RESULT UG_ButtonSetAlternateForeColor(UG_BUTTON *btn, UG_COLOR afc );
-UG_RESULT UG_ButtonSetAlternateBackColor(UG_BUTTON *btn, UG_COLOR abc );
-UG_RESULT UG_ButtonSetText(UG_BUTTON *btn, char* str );
-UG_RESULT UG_ButtonSetFont(UG_BUTTON *btn, const UG_FONT* font );
-UG_RESULT UG_ButtonSetStyle(UG_BUTTON *btn, UG_U8 style );
-UG_RESULT UG_ButtonSetHSpace(UG_BUTTON *btn, UG_S8 hs );
-UG_RESULT UG_ButtonSetVSpace(UG_BUTTON *btn, UG_S8 vs );
-UG_RESULT UG_ButtonSetAlignment(UG_BUTTON *btn, UG_U8 align );
-UG_COLOR UG_ButtonGetForeColor(UG_BUTTON *btn);
-UG_COLOR UG_ButtonGetBackColor(UG_BUTTON *btn);
-UG_COLOR UG_ButtonGetAlternateForeColor(UG_BUTTON *btn);
-UG_COLOR UG_ButtonGetAlternateBackColor(UG_BUTTON *btn);
-char* UG_ButtonGetText(UG_BUTTON *btn);
-UG_FONT* UG_ButtonGetFont(UG_BUTTON *btn);
-UG_U8 UG_ButtonGetStyle(UG_BUTTON *btn);
-UG_S8 UG_ButtonGetHSpace(UG_BUTTON *btn);
-UG_S8 UG_ButtonGetVSpace(UG_BUTTON *btn);
-UG_U8 UG_ButtonGetAlignment(UG_BUTTON *btn);
+UG_RESULT UG_ButtonCreate(UG_BUTTON * btn, UG_WINDOW * wnd, UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye);
+UG_RESULT UG_ButtonDelete(UG_BUTTON * btn);
+UG_RESULT UG_ButtonShow(UG_BUTTON * btn);
+UG_RESULT UG_ButtonHide(UG_BUTTON * btn);
+UG_RESULT UG_ButtonSetForeColor(UG_BUTTON * btn, UG_COLOR fc);
+UG_RESULT UG_ButtonSetBackColor(UG_BUTTON * btn, UG_COLOR bc);
+UG_RESULT UG_ButtonSetAlternateForeColor(UG_BUTTON * btn, UG_COLOR afc);
+UG_RESULT UG_ButtonSetAlternateBackColor(UG_BUTTON * btn, UG_COLOR abc);
+UG_RESULT UG_ButtonSetText(UG_BUTTON * btn, char * str);
+UG_RESULT UG_ButtonSetFont(UG_BUTTON * btn,
+    const UG_FONT * font);
+UG_RESULT UG_ButtonSetStyle(UG_BUTTON * btn, UG_U8 style);
+UG_RESULT UG_ButtonSetHSpace(UG_BUTTON * btn, UG_S8 hs);
+UG_RESULT UG_ButtonSetVSpace(UG_BUTTON * btn, UG_S8 vs);
+UG_RESULT UG_ButtonSetAlignment(UG_BUTTON * btn, UG_U8 align);
+UG_COLOR UG_ButtonGetForeColor(UG_BUTTON * btn);
+UG_COLOR UG_ButtonGetBackColor(UG_BUTTON * btn);
+UG_COLOR UG_ButtonGetAlternateForeColor(UG_BUTTON * btn);
+UG_COLOR UG_ButtonGetAlternateBackColor(UG_BUTTON * btn);
+char * UG_ButtonGetText(UG_BUTTON * btn);
+UG_FONT * UG_ButtonGetFont(UG_BUTTON * btn);
+UG_U8 UG_ButtonGetStyle(UG_BUTTON * btn);
+UG_S8 UG_ButtonGetHSpace(UG_BUTTON * btn);
+UG_S8 UG_ButtonGetVSpace(UG_BUTTON * btn);
+UG_U8 UG_ButtonGetAlignment(UG_BUTTON * btn);
 
 /* Checkbox functions */
-UG_RESULT UG_CheckboxCreate(UG_CHECKBOX* btn, UG_WINDOW* wnd, UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye );
-UG_RESULT UG_CheckboxDelete(UG_CHECKBOX *btn);
-UG_RESULT UG_CheckboxShow(UG_CHECKBOX *btn);
-UG_RESULT UG_CheckboxHide(UG_CHECKBOX *btn);
-UG_RESULT UG_CheckboxSetCheched(UG_CHECKBOX *btn, UG_U8 ch );
-UG_RESULT UG_CheckboxSetForeColor(UG_CHECKBOX *btn, UG_COLOR fc );
-UG_RESULT UG_CheckboxSetBackColor(UG_CHECKBOX *btn, UG_COLOR bc );
-UG_RESULT UG_CheckboxSetAlternateForeColor(UG_CHECKBOX *btn, UG_COLOR afc );
-UG_RESULT UG_CheckboxSetAlternateBackColor(UG_CHECKBOX *btn, UG_COLOR abc );
-UG_RESULT UG_CheckboxSetText(UG_CHECKBOX *btn, char* str );
-UG_RESULT UG_CheckboxSetFont(UG_CHECKBOX *btn, const UG_FONT* font );
-UG_RESULT UG_CheckboxSetStyle(UG_CHECKBOX *btn, UG_U8 style );
-UG_RESULT UG_CheckboxSetHSpace(UG_CHECKBOX *btn, UG_S8 hs );
-UG_RESULT UG_CheckboxSetVSpace(UG_CHECKBOX *btn, UG_S8 vs );
-UG_RESULT UG_CheckboxSetAlignment(UG_CHECKBOX *btn, UG_U8 align );
-UG_U8 UG_CheckboxGetChecked(UG_CHECKBOX *btn);
-UG_COLOR UG_CheckboxGetForeColor(UG_CHECKBOX *btn);
-UG_COLOR UG_CheckboxGetBackColor(UG_CHECKBOX *btn);
-UG_COLOR UG_CheckboxGetAlternateForeColor(UG_CHECKBOX *btn);
-UG_COLOR UG_CheckboxGetAlternateBackColor(UG_CHECKBOX *btn);
-char* UG_CheckboxGetText(UG_CHECKBOX *btn);
-UG_FONT* UG_CheckboxGetFont(UG_CHECKBOX *btn);
-UG_U8 UG_CheckboxGetStyle(UG_CHECKBOX *btn);
-UG_S8 UG_CheckboxGetHSpace(UG_CHECKBOX *btn);
-UG_S8 UG_CheckboxGetVSpace(UG_CHECKBOX *btn);
-UG_U8 UG_CheckboxGetAlignment(UG_CHECKBOX *btn);
+UG_RESULT UG_CheckboxCreate(UG_CHECKBOX * btn, UG_WINDOW * wnd, UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye);
+UG_RESULT UG_CheckboxDelete(UG_CHECKBOX * btn);
+UG_RESULT UG_CheckboxShow(UG_CHECKBOX * btn);
+UG_RESULT UG_CheckboxHide(UG_CHECKBOX * btn);
+UG_RESULT UG_CheckboxSetCheched(UG_CHECKBOX * btn, UG_U8 ch);
+UG_RESULT UG_CheckboxSetForeColor(UG_CHECKBOX * btn, UG_COLOR fc);
+UG_RESULT UG_CheckboxSetBackColor(UG_CHECKBOX * btn, UG_COLOR bc);
+UG_RESULT UG_CheckboxSetAlternateForeColor(UG_CHECKBOX * btn, UG_COLOR afc);
+UG_RESULT UG_CheckboxSetAlternateBackColor(UG_CHECKBOX * btn, UG_COLOR abc);
+UG_RESULT UG_CheckboxSetText(UG_CHECKBOX * btn, char * str);
+UG_RESULT UG_CheckboxSetFont(UG_CHECKBOX * btn,
+    const UG_FONT * font);
+UG_RESULT UG_CheckboxSetStyle(UG_CHECKBOX * btn, UG_U8 style);
+UG_RESULT UG_CheckboxSetHSpace(UG_CHECKBOX * btn, UG_S8 hs);
+UG_RESULT UG_CheckboxSetVSpace(UG_CHECKBOX * btn, UG_S8 vs);
+UG_RESULT UG_CheckboxSetAlignment(UG_CHECKBOX * btn, UG_U8 align);
+UG_U8 UG_CheckboxGetChecked(UG_CHECKBOX * btn);
+UG_COLOR UG_CheckboxGetForeColor(UG_CHECKBOX * btn);
+UG_COLOR UG_CheckboxGetBackColor(UG_CHECKBOX * btn);
+UG_COLOR UG_CheckboxGetAlternateForeColor(UG_CHECKBOX * btn);
+UG_COLOR UG_CheckboxGetAlternateBackColor(UG_CHECKBOX * btn);
+char * UG_CheckboxGetText(UG_CHECKBOX * btn);
+UG_FONT * UG_CheckboxGetFont(UG_CHECKBOX * btn);
+UG_U8 UG_CheckboxGetStyle(UG_CHECKBOX * btn);
+UG_S8 UG_CheckboxGetHSpace(UG_CHECKBOX * btn);
+UG_S8 UG_CheckboxGetVSpace(UG_CHECKBOX * btn);
+UG_U8 UG_CheckboxGetAlignment(UG_CHECKBOX * btn);
 
 /* Textbox functions */
-UG_RESULT UG_TextboxCreate(UG_TEXTBOX* txb, UG_WINDOW* wnd, UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye );
-UG_RESULT UG_TextboxDelete(UG_TEXTBOX *txb);
-UG_RESULT UG_TextboxShow(UG_TEXTBOX *txb);
-UG_RESULT UG_TextboxHide(UG_TEXTBOX *txb);
-UG_RESULT UG_TextboxSetForeColor(UG_TEXTBOX *txb, UG_COLOR fc );
-UG_RESULT UG_TextboxSetBackColor(UG_TEXTBOX *txb, UG_COLOR bc );
-UG_RESULT UG_TextboxSetText(UG_TEXTBOX *txb, char* str );
-UG_RESULT UG_TextboxSetFont(UG_TEXTBOX *txb, const UG_FONT* font );
-UG_RESULT UG_TextboxSetHSpace(UG_TEXTBOX *txb, UG_S8 hs );
-UG_RESULT UG_TextboxSetVSpace(UG_TEXTBOX *txb, UG_S8 vs );
-UG_RESULT UG_TextboxSetAlignment(UG_TEXTBOX *txb, UG_U8 align );
-UG_COLOR UG_TextboxGetForeColor(UG_TEXTBOX *txb);
-UG_COLOR UG_TextboxGetBackColor(UG_TEXTBOX *txb);
-char* UG_TextboxGetText(UG_TEXTBOX *txb);
-UG_FONT* UG_TextboxGetFont(UG_TEXTBOX *txb);
-UG_S8 UG_TextboxGetHSpace(UG_TEXTBOX *txb);
-UG_S8 UG_TextboxGetVSpace(UG_TEXTBOX *txb);
-UG_U8 UG_TextboxGetAlignment(UG_TEXTBOX *txb);
+UG_RESULT UG_TextboxCreate(UG_TEXTBOX * txb, UG_WINDOW * wnd, UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye);
+UG_RESULT UG_TextboxDelete(UG_TEXTBOX * txb);
+UG_RESULT UG_TextboxShow(UG_TEXTBOX * txb);
+UG_RESULT UG_TextboxHide(UG_TEXTBOX * txb);
+UG_RESULT UG_TextboxSetForeColor(UG_TEXTBOX * txb, UG_COLOR fc);
+UG_RESULT UG_TextboxSetBackColor(UG_TEXTBOX * txb, UG_COLOR bc);
+UG_RESULT UG_TextboxSetText(UG_TEXTBOX * txb, char * str);
+UG_RESULT UG_TextboxSetFont(UG_TEXTBOX * txb, const UG_FONT * font);
+UG_RESULT UG_TextboxSetHSpace(UG_TEXTBOX * txb, UG_S8 hs);
+UG_RESULT UG_TextboxSetVSpace(UG_TEXTBOX * txb, UG_S8 vs);
+UG_RESULT UG_TextboxSetAlignment(UG_TEXTBOX * txb, UG_U8 align);
+UG_COLOR UG_TextboxGetForeColor(UG_TEXTBOX * txb);
+UG_COLOR UG_TextboxGetBackColor(UG_TEXTBOX * txb);
+char * UG_TextboxGetText(UG_TEXTBOX * txb);
+UG_FONT * UG_TextboxGetFont(UG_TEXTBOX * txb);
+UG_S8 UG_TextboxGetHSpace(UG_TEXTBOX * txb);
+UG_S8 UG_TextboxGetVSpace(UG_TEXTBOX * txb);
+UG_U8 UG_TextboxGetAlignment(UG_TEXTBOX * txb);
 
 /* Image functions */
-UG_RESULT UG_ImageCreate(UG_IMAGE *img, UG_WINDOW *wnd, UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye);
-UG_RESULT UG_ImageDelete(UG_IMAGE *img);
-UG_RESULT UG_ImageShow(UG_IMAGE *img);
-UG_RESULT UG_ImageHide(UG_IMAGE *img);
-UG_RESULT UG_ImageSetBMP(UG_IMAGE *img, const UG_BMP *bmp);
+UG_RESULT UG_ImageCreate(UG_IMAGE * img, UG_WINDOW * wnd, UG_S16 xs, UG_S16 ys, UG_S16 xe, UG_S16 ye);
+UG_RESULT UG_ImageDelete(UG_IMAGE * img);
+UG_RESULT UG_ImageShow(UG_IMAGE * img);
+UG_RESULT UG_ImageHide(UG_IMAGE * img);
+UG_RESULT UG_ImageSetBMP(UG_IMAGE * img, const UG_BMP * bmp);
 
 /* MQ 10/18/2017  Nordic HW acceleration functions via nrf_gfx.h */
 #ifdef NORDIC_GUI
 #include "nrf_gfx_ext.h"
 static UG_RESULT _HW_draw_line(UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c);
-static UG_RESULT _HW_fill_frame( UG_S16 x1, UG_S16 y1, UG_S16 x2 , UG_S16 y2 , UG_COLOR c);
-static UG_RESULT _HW_draw_buffer(UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, void *p_data, UG_U32 length);
-UG_S16 UG_Init( UG_GUI* g, UG_S16 x, UG_S16 y, const nrf_lcd_t *p_nrf_lcd);
-static void UserSetPixel (UG_S16 x, UG_S16 y, UG_COLOR c);
-static const nrf_lcd_t *p_lcd;
+static UG_RESULT _HW_fill_frame(UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c);
+static UG_RESULT _HW_draw_buffer(UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, void * p_data, UG_U32 length);
+UG_S16 UG_Init(UG_GUI * g, UG_S16 x, UG_S16 y, const nrf_lcd_t * p_nrf_lcd);
+static void UserSetPixel(UG_S16 x, UG_S16 y, UG_COLOR c);
+static const nrf_lcd_t * p_lcd;
 #endif
 
 
